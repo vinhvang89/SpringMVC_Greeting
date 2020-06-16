7cb27df51b6db027bce5f720ac9574395ea8af52
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-06-16 11:27:54 +0700

    hehe

--- src/controllers/ControllerGreeting.java
+++ src/controllers/ControllerGreeting.java
@@ -1,4 +1,6 @@
 package controllers;
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ControllerGreeting {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam String name , Model model){
+    public String greeting(@RequestParam String name, Model model){
         model.addAttribute("name",name);
         return "index";
     }
